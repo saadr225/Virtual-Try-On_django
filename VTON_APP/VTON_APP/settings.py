06856829b36787cbe706bc8fde92f82f4d312b25
Saad Rehan <saadrehan1000@gmail.com>
2025-10-22 22:06:44 +0500

    Update environment configuration in .env.example and settings.py to use environment variables for DEBUG and ALLOWED_HOSTS

--- VTON_APP/VTON_APP/settings.py
+++ VTON_APP/VTON_APP/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ih*8oi)ve^r++cr@-(5n)_y4j=^zf%soxebz+r^xi4+$kt!!f7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
